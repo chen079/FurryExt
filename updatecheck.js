804ee10c47c991e1ab42f0310d3e9b0972862378
chen079 <2428701101@qq.com>
2023-05-14 09:16:37 +0800

    update!

--- updatecheck.js
+++ updatecheck.js
@@ -1,19 +1,16 @@
 (() => ({
 	// 扩展版本
-	version: "2.2.0.4",
+	version: "2.2.0.5",
 
 	// 扩展的上一个版本
-	oldversion: "2.2.0.3",
+	oldversion: "2.2.0.4",
 
 	// 更新内容汇总
 	changeLog: `
-	新角色 多林
-	新角色 迪斯
-	新系统 成就奖励
-	新成就 拆弹成功、虚争空言，不如击而破之、神仙难救
-	修改 tails 转移选项
-	修改 rasali 灵引技能
-	修改 zhan 的部分标记错误
+	修复各种错误
+	修复部分成就无法获得的错误
+	重写ChooseText，解决不会自动换人的错误
+	重做人物萨伊苏
     `,
 
 	// 本次更新的所有文件(不包括文件夹)
@@ -26,58 +23,9 @@
 		'asset/drama.js',
 		'asset/furry_achievement.js',
 		'extension.js',
-		'image/card/fr_card_xzst.png',
-		'image/card/pretty/fr_card_xzst.jpg',
-		'image/card/pretty/fr_card_xzst.bmp',
-		'image/card/pretty/fr_card_xzst.webp',
-		'image/card/pretty/fr_card_xzst.png',
-		'image/lutou/fr_derk.jpg',
-		'image/character/fr_derk.jpg',
-		'image/character/fr_thunder.jpg',
-		'image/lutou/fr_thunder.jpg',
-		'image/biaoqian/mark_furry.png',
-		'image/achievement/arrow.png',
-		'image/achievement/character.png',
-		'image/achievement/frame.png',
-		'image/achievement/lit.png',
-		'image/achievement/lock.png',
-		'image/achievement/receive.png',
-		'image/achievement/received.png',
-		'image/achievement/skin.png',
-		'image/achievement/unlit.png',
-		'image/character/fr_death.jpg',
-		'image/character/fr_dolina.jpg',
-		'image/character/fr_ming.jpg',
-		'image/lutou/fr_death.jpg',
-		'image/lutou/fr_dolina.jpg',
-		'image/lutou/fr_ming.jpg',
-		'image/character/fr_wind.jpg',
-		'image/lutou/fr_ming.jpg',
-		'image/reward/reward_fr_aoeslat.png',
-		'image/reward/reward_fr_crow.png',
-		'image/reward/reward_fr_jiejie.png',
-		'image/reward/reward_fr_ken.png',
-		'image/reward/reward_fr_klif.png',
-		'image/reward/reward_fr_ming.png',
-		'image/reward/reward_fr_muli.png',
-		'image/reward/reward_fr_muyada.png',
-		'image/reward/reward_fr_shark.png',
-		'image/reward/reward_fr_thunder.png',
-		'image/reward/reward_fr_wes.png',
-		'image/reward/reward_休闲时光.png',
-		'image/reward/reward_异界之花.png',
-		'image/reward/reward_战场蔷薇.png',
-		'image/reward/reward_树影斑驳.png',
-		'image/reward/reward_死亡降临.png',
-		'image/reward/reward_碾碎时光.png',
-		'skin/lutou/fr_death/死亡降临.jpg',
-		'skin/standard/fr_death/死亡降临.jpg',
-		'skin/lutou/fr_dolina/一剑横江.jpg',
-		'skin/lutou/fr_dolina/休闲时光.jpg',
-		'skin/lutou/fr_dolina/分海裂空.jpg',
-		'skin/standard/fr_dolina/一剑横江.jpg',
-		'skin/standard/fr_dolina/休闲时光.jpg',
-		'skin/standard/fr_dolina/分海裂空.jpg',
+		'image/qidongye/furry_lib.jpg',
+		'image/qidongye/furry_lib2.jpg',
+		'image/audio/drama/Throw_coin.mp3',
 	],
 	// 扩展内所有文件(不包括文件夹)
 	allFiles: [
