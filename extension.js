851f3479a1b278e37905925097897d946792133c
chen079 <2428701101@qq.com>
2023-11-02 18:14:23 +0800

    uopdatE!

--- extension.js
+++ extension.js
@@ -1846,7 +1846,7 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
                 + '<div id="active" style="text-align: center;width: 100%;border: double;border-radius: 3px;padding-bottom: 5px;"><div>使用福利码</div><br><input type="text" name="activeKey" placeholder="请输入福利码"/>&nbsp&nbsp<button id="activeKey">激活</button></div>',
             diskURL: "",
             forumURL: "",
-            version: "3.0",
+            version: "3.0.1",
         },
         files: {
             "character": [],
